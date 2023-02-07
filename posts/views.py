4e405ae91530c18c124547afe49d7e064b53da05
Mitesh Pawanarkar <pawanarkamitesh28@gmail.com>
2023-02-07 20:50:58 +0530

    This is my commit

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.views import generic
-
 from braces.views import SelectRelatedMixin
 from . import models
 from . import forms 
-
 from django.contrib.auth import get_user_model
 User=get_user_model()
 
